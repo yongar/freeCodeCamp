fb6b764282f9d98f555c8ca654c0ac61e3f8311e
Yong Choi <yongar@gmail.com>
2017-01-28 20:21:10 -0800

    a

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
-$ch = curl_init('https://ipinvestigator.expeditedaddons.com/?api_key=' . getenv('IPINVESTIGATOR_API_KEY') . '&ip=68.10.149.45');
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-var_dump($response);
+header('Refresh: 0; url=project/bootstrap/index.html');
 ?>
 
 <? //phpinfo() ?>
