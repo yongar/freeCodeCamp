c922f6064f0084980690714f97cfa5ad9927a601
Yong Choi <yongar@gmail.com>
2017-02-16 16:08:56 -0800

    a

---
+++ first_server.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+function process_request (req, res) {
+	var body = "thanks for calling \n";
+	var content_length = body.length;
+	res.writeHead(200, {
+		'Content-Length' : content_length,
+		'Content-Type': 'text/plain',
+	});
+	res.end(body);
+}
+
+
+var s= http.createServer(process_request);
+s.listen(8000);
