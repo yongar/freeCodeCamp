1adf19592110666f7fff8dca5a0b7a82e0d049b3
Yong Choi <yongar@gmail.com>
2017-02-17 13:06:19 -0800

    a

--- destroyer.js
+++ destroyer.js
@@ -3,6 +3,16 @@ function removeItem(value, item){
 }
 
 function destroyer(arr) {
+var args = Array.prototype.slice.call(arguments);
+  args.splice(0, 1);
+  return arr.filter(function(element) {
+    return args.indexOf(element) === -1;
+  });
+}
+
+
+
+function mydestroyer(arr) {
 	var result = arguments;
 	console.log(result);
 	var ar = arguments[0];
---
+++ getIntexToIns.js
@@ -0,0 +1,7 @@
+function getIndexToIns(arr, num) {
+  arr.sort();
+  console.log(arr);
+  return num;
+}
+
+getIndexToIns([40, 60], 50);
