aadc57bc5058b323eb6127b6dc326af6ee97f5fc
yongar <yongar@Yongs-MacBook-Pro.local>
2017-02-27 17:13:25 -0800

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
-    "extends": "standard",
+    "extends": "eslint:recommended",
+    "env":{
+        "node" :true, 
+        "jquery": true,
+        "browser": true
+    } ,
     "plugins": [
-        "standard",
-        "promise"
-    ]
+        "import"
+    ],
+    "rules":{
+
+        "linebreak-style":0,
+        "indent" : ["error", "tab"],
+        "quotes": ["error", "double"],
+        "semi" : ["error", "always", { "omitLastInOneLineBlock": true }],
+        "comma-dangle" :["error", "never"],
+        "curly": ["error", "all"],
+        "no-console" : "off",
+        "strict": "off"
+
+    }
 };
--- excercise/arrayMap.js
+++ excercise/arrayMap.js
@@ -1,6 +1,6 @@
-var oldArray = [1, 2, 3, 4, 5];
+var oldArray = [1, 2, 3, 4, 5, ];
 // Only change code below this line.
-var add3 = oldArray.map(function (val) {
+var add3 = oldArray.map((val) => {
     var result = val + 3;
     console.log(result);
     return result;
