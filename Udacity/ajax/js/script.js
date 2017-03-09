6f42617fb86f605a661139e19657d479e027ac0e
Yong Choi <yongar@imac.local>
2017-03-08 20:58:49 -0800

    a

--- Udacity/ajax/js/script.js
+++ Udacity/ajax/js/script.js
@@ -31,23 +31,67 @@ function loadData() {
         'q': address,
         'Access-Control-Allow-Origin': true
     });
+    var nyTimesTimeOut = setTimeout(function () {
+        $nytHeaderElem.text('failed to get NYT article')
 
+    })
 
     $.getJSON(NYtimesURL, function (data) {
-        //        console.log(data);
-        $nytHeaderElem.text("New York Time Article about " + cityStr);
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article"> ' + '<a href="' + article.web_url +
-                '" target="_blank">' + article.headline.main + '</a><br>by ' +
-                article.byline + '<br> <p>' + article.snippet + '</p>' +
-                '</li>');
-        }
+            //        console.log(data);
+            $nytHeaderElem.text("New York Time Article about " + cityStr);
+            articles = data.response.docs;
+            for (var i = 0; i < articles.length; i++) {
+                var article = articles[i];
+                $nytElem.append('<li class="article"> ' + '<a href="' + article.web_url +
+                    '" target="_blank">' + article.headline.main + '</a><br>by ' +
+                    article.byline + '<br> <p>' + article.snippet + '</p>' +
+                    '</li>');
+            }
 
-    }).error(function (e) {
-        $nytHeaderElem.text("does not work");
-    });
+
+
+        })
+        .done(function () {
+            clearTimeout(nyTimesTimeOut);
+        })
+        .error(function (e) {
+            $nytHeaderElem.text("does not work");
+        });
+    // Wikipedia
+    $.ajax({
+            url: 'https://en.wikipedia.org/w/api.php',
+            method: 'POST',
+            dataType: 'jsonp',
+            "Access-Control-Allow-Origin": true,
+            data: {
+                action: 'opensearch',
+                search: cityStr,
+                generator: 'search',
+                format: 'json'
+
+            }
+
+        })
+        .done(function (response) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleList[i]
+                var articleStr = articleList[i];
+                var url = "https://en.wikipedia.org/wiki/" + articleStr;
+                $wikiElem.append('<li><a href="' + url + '" target="_blank">' + articleStr +
+                    '</a></li>');
+            }
+
+
+        })
+        .fail(function () {
+            $wikiElem.text('failed to get wikipedia articles');
+        })
+        .always(function () {
+            console.log("complete");
+        });
+
 
     return false;
 }
