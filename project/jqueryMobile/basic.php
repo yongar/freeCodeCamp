f1dd8c62b77bd796b2a50411690545b1ad621b01
Yong Choi <yongar@imac.local>
2017-03-12 22:15:31 -0700

    mobile detection and http://idangero.us/swiper/demos/ integration

--- project/jqueryMobile/basic.php
+++ project/jqueryMobile/basic.php
@@ -1,10 +1,33 @@
+<?php
+include_once 'vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
+
+$detect = new Mobile_Detect;
+// Jquery first
+echo '	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>';
+// then Jquery Mobile if required.
+
+if ($detect->isMobile()){
+	 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>';
+	 echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.min.js"></script>';
+}
+
+// Redirect the user to your mobile version of the site.
+// if($detect->isMobile()){
+//     header('http://m.yoursite.com', true, 301);
+// }
+// example
+// https://github.com/serbanghita/Mobile-Detect/wiki/Code-examples
+
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.css">
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
 	<script type="text/javascript" src="/js/bootstrap.min.js">
 	</script>
 	<style type="text/css" media="screen">
@@ -13,17 +36,7 @@
 		background-color: red;
 	}
 	</style>
-	<?php
-	$ua = $_SERVER['HTTP_USER_AGENT'];
-	echo $ua;
-    if ( stristr($_SERVER['HTTP_USER_AGENT'], "Mobile" )) {
-        $DEVICE_TYPE="MOBILE";
-    }
 
-    if (isset($DEVICE_TYPE) and $DEVICE_TYPE=="MOBILE") {
-        echo '<script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>';
-    }
-    ?>
 	<title>External Page1 </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
@@ -33,7 +46,25 @@
 	<a href="basic.html" title="don't click here">don't click here</a>
 	<div id="div1">Click Here</div>
 	<div id="div2">Double Click here </div>
+<div class="swiper-container">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+        <!-- Slides -->
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+        ...
+    </div>
+    <!-- If we need pagination -->
+    <div class="swiper-pagination"></div>
 
+    <!-- If we need navigation buttons -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+
+    <!-- If we need scrollbar -->
+    <div class="swiper-scrollbar"></div>
+</div>
 	<script>
 	$("a:first").attr("href", "http://www.yongar.xyz");
 	$("a:first").attr("target", "_blank");
@@ -53,6 +84,21 @@
 		$(this).removeClass('text-primary');
 		$(this).addClass('text-muted');
 	});
+	var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: '.swiper-pagination',
+
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+
+    // And if we need scrollbar
+    scrollbar: '.swiper-scrollbar',
+  })
 	</script>
 	<?php
 
