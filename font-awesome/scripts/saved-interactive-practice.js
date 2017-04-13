d3b2721ca5b74b4b65a6cfe646f428180ac53461
yongar <yongar@Yongs-MacBook-Pro.local>
2017-04-12 20:04:31 -0700

    a

--- font-awesome/scripts/saved-interactive-practice.js
+++
@@ -1,9 +0,0 @@
-function logText(text) { console.log("Text from server is %o.", text); }
-
-logText("Example from server");
-
-$.ajax({ url: "server-result.txt", success: logText });
-
-function alertText(text) { alert("Text from server is " + text); }
-
-$.ajax({ url: "server-result.txt", success: alertText });
